b0b82e6f6c54d9951ffa0757a294aa633eafed16
u19236931 <103131703+u19236931@users.noreply.github.com>
2022-06-08 23:57:16 +0200

    Add files via upload

---
+++ Controllers/FileDownloadController.cs
@@ -0,0 +1,60 @@
+﻿using FileUploading.Models; // If we do not have this then it is
+
+// List<Models.FileModel> files = new List<Models.FileModel>();
+
+using System.Collections.Generic; // Helps with lists
+
+using System.IO; // Required for reading and writing IO (Input / Output).
+using System.Web.Mvc;
+
+namespace FileUploading.Controllers
+{
+    public class FileDownloadController : Controller 
+    {
+        
+        public ActionResult Index()
+        {
+            
+
+            string[] filePaths = Directory.GetFiles(Server.MapPath("~/App_Data/uploads/"));
+
+            
+
+            List<FileModel> files = new List<FileModel>();
+            
+
+            foreach (string filePath in filePaths)
+            {
+                files.Add(new FileModel { FileName = Path.GetFileName(filePath) });
+            }
+            return View(files);
+        }
+
+        public FileResult DownloadFile(string fileName) 
+        {
+            
+
+            string path = Server.MapPath("~/App_Data/uploads/") + fileName;
+
+            
+
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+
+            
+
+            return File(bytes, "application/octet-stream", fileName);
+        }
+
+        public ActionResult DeleteFile(string fileName)
+        {
+            
+
+            string path = Server.MapPath("~/App_Data/uploads/") + fileName;
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+
+            System.IO.File.Delete(path);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+using System.IO;
+using System.Web;
+
+namespace FileUploading.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase files, string type)  
+        {
+            
+
+            if (files != null && files.ContentLength > 0)
+            {
+                
+                Debug.WriteLine(type);
+                if (type == "Document")
+                {
+
+
+
+                    
+
+                    var fileName = Path.GetFileName(files.FileName);
+
+                  
+
+                    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+
+                    
+
+                    files.SaveAs(path);
+                }
+                
+                if (type == "Image")
+                {
+
+
+
+                   
+
+                    var fileName = Path.GetFileName(files.FileName);
+
+                    
+
+                    var path = Path.Combine(Server.MapPath("~/App_Data/images"), fileName);
+
+                   
+
+                    files.SaveAs(path);
+                }
+                
+                if (type == "Video")
+                {
+
+
+
+                    
+
+                    var fileName = Path.GetFileName(files.FileName);
+
+                    
+
+                    var path = Path.Combine(Server.MapPath("~/App_Data/videos"), fileName);
+
+                    
+
+                    files.SaveAs(path);
+                }
+
+
+
+
+
+            }
+            
+
+            return RedirectToAction("Index");
+        }
+        
+        
+    }
+}
